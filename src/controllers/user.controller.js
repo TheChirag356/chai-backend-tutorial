7c84f359e3713acd9b14397b22de8a13cc00ef70
TheChirag356 <arorachirag12005@gmail.com>
2024-08-20 23:48:05 +0530

    feat: Add endpoint to fetch user channel profile

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -371,6 +371,77 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, user, "Avatar updated successfully"));
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username?.trim()) {
+      throw new ApiError(400, "Username is missing");
+  }
+
+  // const user = User.find({})
+
+  const channel = await User.aggregate([
+      {
+          $match: {
+              username: username?.toLowerCase(),
+          },
+      },
+      {
+          $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "channel",
+              as: "subscribers",
+          },
+      },
+      {
+          $lookup: {
+              from: "subscriptions",
+              localField: "_id",
+              foreignField: "subscriber",
+              as: "subscribedTo",
+          },
+      },
+      {
+        $addFields: {
+          subscribersCount: {
+            $size: "$subscribers"
+          },
+          channelsSubscribedToCount: {
+            $size: "$subscribedTo"
+          },
+          isSubscribed: {
+            $cond: {
+              if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+              then: true,
+              else: false
+            }
+          }
+        }
+      },
+      {
+        $project: {
+          fullName: 1,
+          username: 1,
+          subscribersCount: 1,
+          channelsSubscribedToCount: 1,
+          isSubscribed: 1,
+          avatar: 1,
+          coverImage: 1,
+        }
+      }
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exist")
+  }
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200, channel[0], "User Channel Fetched Successfully")
+  )
+});
 
 export {
     registerUser,
