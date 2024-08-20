b079bb538298e3d1cf89a47e19b7fba3eda323a5
TheChirag356 <arorachirag12005@gmail.com>
2024-08-20 19:14:16 +0530

    feat: Fix import typo in cloudinary.js file

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
-import { ApiError } from "./ApiError";
+import { ApiError } from "./ApiError.js";
 
 // Configuration
 cloudinary.config({
@@ -28,7 +28,7 @@ const uploadOnCloudinary = async (localFilePath) => {
 
 const removeImageFromCloudinary = async (oldURL) => {
   try {
-    if (!oldrURL) {
+    if (!oldURL) {
       throw new ApiError(400, "Invalid avatar image url");
     }
 
